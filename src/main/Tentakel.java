237808189cde3d7bf729194b0ebbb5e5803a4315
judith <judith.conradi@gmail.com>
2014-05-19 16:37:03 +0200

    ärmchen

--- src/main/Tentakel.java
+++ src/main/Tentakel.java
@@ -7,30 +7,34 @@ public class Tentakel{
 Vektor abstand = new Vektor();
 Vektor pos = new Vektor();
 int nummer;
-int armZahl;
+int armLängenZahl;
 
 	public Tentakel(int num, int krakenRadius){
 		this.nummer = num;
 		abstand.x = 0;
 		abstand.y = -krakenRadius;
 		abstand.drehen(108+20*nummer);
-		armZahl = 10;
-		for (int i = 0; i<armZahl; i++){
+		armLängenZahl = 10;
+		for (int i = 0; i<armLängenZahl; i++){
 			armKreis[i] = new Kreis(pos, krakenRadius/20); 
 		}
 	}
 	
 	public void update(Vektor krakenPosition){
-		Vektor kPosition = krakenPosition;
-		pos.x = kPosition.x + abstand.x;
-		pos.y = kPosition.y + abstand.y;
+		pos.x = krakenPosition.x + abstand.x;
+		pos.y = krakenPosition.y + abstand.y;
 		//System.out.println("Nummer: " + nummer + "; Position:"+ position.x + "," + position.y);
 	}
 	//Kreis arm = new Kreis(pos, 30);
 	Kreis[] armKreis = new Kreis[10];
 	float tentakelBreite = 30;
+	Vektor position= new Vektor(0,0);
 	public void draw (Graphics g, Color c){
-		for (int i = 0; i<armZahl; i++){
+		position.x = pos.x;
+		position.y = pos.y;
+		for (int i = 0; i<armLängenZahl; i++){
+			pos.x = position.x + abstand.x*i/20;
+			pos.y = position.y + abstand.y*i/20;
 			armKreis[i].draw(g, c, pos); 
 		}
 

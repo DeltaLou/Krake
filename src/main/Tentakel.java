a2ec2030cc86a57faefef7ad9080f1fa3559a1ba
judith <judith.conradi@gmail.com>
2014-05-19 17:51:15 +0200

    süße runde Ärmchen - bewegen sich noch nicht

--- src/main/Tentakel.java
+++ src/main/Tentakel.java
@@ -23,9 +23,13 @@ int armLängenZahl = 30;
 	public void update(Vektor krakenPosition){
 		pos.x = krakenPosition.x + abstand.x;
 		pos.y = krakenPosition.y + abstand.y;
-		for (int i = 0; i<armLängenZahl; i++){
-			armKreis[i].mittelpunkt.x= pos.x + abstand.x*i/10;
-			armKreis[i].mittelpunkt.y= pos.y+abstand.y*i/10;
+		armKreis[0].mittelpunkt.x = pos.x;
+		armKreis[0].mittelpunkt.y = pos.y;
+		for (int i = 1; i<armLängenZahl; i++){
+			//armKreis[i].mittelpunkt.x= pos.x + abstand.x*i/10;
+			//armKreis[i].mittelpunkt.y= pos.y + abstand.y*i/10;
+			armKreis[i].mittelpunkt.x= armKreis[i-1].mittelpunkt.x + abstand.x*armKreis[i].radius/100;
+			armKreis[i].mittelpunkt.y= armKreis[i-1].mittelpunkt.y + abstand.y*armKreis[i].radius/100;
 			}
 		//System.out.println("Nummer: " + nummer + "; Position:"+ pos.x + "," + pos.y);
 	}

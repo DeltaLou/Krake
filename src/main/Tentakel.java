f25f902642acdef3137a949c899efeb3a1793e05
judith <judith.conradi@gmail.com>
2014-05-19 15:31:29 +0200

    Der Versuch, Tentakeln einzubauen. FIX ME!

--- src/main/Tentakel.java
+++ src/main/Tentakel.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 
 public class Tentakel{
 Vektor abstand = new Vektor();
-Vektor position = new Vektor();
+Vektor pos = new Vektor();
 int nummer;
 
 	public Tentakel(int num, int krakenRadius){
@@ -13,15 +13,25 @@ int nummer;
 		abstand.x = 0;
 		abstand.y = -krakenRadius;
 		abstand.drehen(108+20*nummer);
+		for (int i = 0; i<=7; i++){
+			armKreis[i] = new Kreis(pos, krakenRadius/20); 
+		}
 	}
 	
 	public void update(Vektor krakenPosition){
-		position.x = krakenPosition.x + abstand.x;
-		position.y = krakenPosition.y + abstand.y;
+		Vektor kPosition = krakenPosition;
+		pos.x = kPosition.x + abstand.x;
+		pos.y = kPosition.y + abstand.y;
 		//System.out.println("Nummer: " + nummer + "; Position:"+ position.x + "," + position.y);
 	}
-	Kreis arm = new Kreis(position, 30);
+	//Kreis arm = new Kreis(pos, 30);
+	Kreis[] armKreis = new Kreis[10];
+	float tentakelBreite = 30;
 	public void draw (Graphics g, Color c){
-		arm.draw(g, c, position);
+		for (int i = 0; i<=10; i++){
+			armKreis[i].draw(g, c, pos); 
+		}
+
+		//arm.draw(g, c, position);
 	}
 }

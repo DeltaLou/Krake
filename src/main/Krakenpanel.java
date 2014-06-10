7d099fd3445af36c4c263746978b4db3ba8c70ae
judith <judith.conradi@gmail.com>
2014-06-10 16:45:04 +0200

    Hintergrund mit Verlauf

--- src/main/Krakenpanel.java
+++ src/main/Krakenpanel.java
@@ -15,6 +15,16 @@ public class Krakenpanel extends DoubleBuffer {
 	}
 
 	public void paintBuffer(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        int w = getWidth();
+        int h = getHeight();
+        Color color1 = Color.WHITE;
+        Color color2 = Color.BLUE;
+        GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, w, h);
 		polly.draw(g);
+
 	}
 }

198c66c9b62322b080058079534c05d3e2883488
judith <judith.conradi@gmail.com>
2014-06-10 16:39:30 +0200

    lustige hintergrundfehler

--- src/main/DoubleBuffer.java
+++ src/main/DoubleBuffer.java
@@ -22,6 +22,14 @@ public class DoubleBuffer extends Panel {
 
 	@Override
 	public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        int w = getWidth();
+        int h = getHeight();
+        Color color1 = Color.WHITE;
+        Color color2 = Color.BLUE;
+        GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
+        g2d.setPaint(gp);
 		// checks the buffersize with the current panelsize
 		// or initialises the image with the first paint
 		if (bufferWidth != getSize().width || bufferHeight != getSize().height
@@ -33,6 +41,7 @@ public class DoubleBuffer extends Panel {
 
 			// we finaly paint the offscreen image onto the onscreen image
 			paintBuffer(bufferGraphics);
+	        g2d.fillRect(0, 0, w, h);
 			g.drawImage(bufferImage, 0, 0, this);
 		}
 	}
